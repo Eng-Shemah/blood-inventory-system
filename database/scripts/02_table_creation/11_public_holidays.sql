144e45fdf44094c1ac14085a45ff169b1124aa21
Eng-Shemah <shemagwizarobert250@gmail.com>
2025-12-21 00:46:20 +0200

    Create public_holidays table with relevant fields

---
+++ database/scripts/02_table_creation/11_public_holidays.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public_holidays (
+    holiday_id NUMBER PRIMARY KEY,
+    holiday_name VARCHAR2(100) NOT NULL,
+    holiday_date DATE NOT NULL,
+    country VARCHAR2(50) DEFAULT 'Rwanda',
+    is_active CHAR(1) DEFAULT 'Y' CHECK (is_active IN ('Y', 'N')),
+    created_date DATE DEFAULT SYSDATE,
+    CONSTRAINT uk_holiday_date UNIQUE (holiday_date, country)
+);
